dd3f7ddf2044de2b4fa01f5fd795b6fdea4626a1
shibeyshi <144595176+shib-a@users.noreply.github.com>
2024-05-18 01:24:26 +0400

    Update lab7_test.asm

--- lab7_test.asm
+++ lab7_test.asm
@@ -1,11 +1,22 @@
 org 0x426
 arg_1_1: word 0x0
 arg_1_2: word 0xFFFF
+arg_2_1: word 0x1234
+arg_2_2: word 0x4321
+arg_3: word 0x8A69
 res: word 0x0
 res_1: word 0xFFFF
+res_2: word 0x4334
+res_3: word 0x8A69
+res_4: word 0x437
 main:
-	call arg_1_1
-	ld res
+	call test_1
+	hlt
+	call test_2
+	hlt
+	call test_3
+	hlt
+	call test_4 
 	hlt
 test_1:
 	ld arg_1_1
@@ -13,18 +24,85 @@ test_1:
 	ld arg_1_2
 	push
 	word 0x0F01
+	nop
 	cmp res_1
-	beq correct
-	ld #0
+	beq correct_1
+incorrect_1:
+	ld #0x0
 	st res
+	jump exit
+correct_1:	bnc incorrect_1
+ 	ld res
+ 	inc
+ 	st res
+ 	jump exit
 exit:
  	pop
  	pop
  	cla
  	ret
- correct:
+test_2:
+	ld arg_2_1
+	push
+	ld arg_2_2
+	push
+	word 0x0F01
+	nop
+	cmp res_2
+	beq correct_2
+incorrect_2:
+	ld #0x0
+	st res
+	jump exit_2
+ correct_2:	bns incorrect_2
  	ld res
  	inc
  	st res
- 	beq exit
- 	
+ 	jump exit_2
+exit_2:
+	pop
+	pop
+	cla
+	ret
+test_3: 
+	ld arg_3
+	push
+	push
+	word 0x0F01
+	nop
+	cmp res_3
+	beq correct_3
+incorrect_3:
+	ld #0x0
+	st res
+	jump exit_3
+ correct_3:	bnc incorrect_3
+ 	ld res
+ 	inc
+ 	st res
+ 	jump exit_3
+exit_3:
+	pop
+	pop
+	cla
+	ret
+test_4: 
+	ld #0x0
+	push
+	word 0x0F01
+	nop
+	cmp res_4
+	beq correct_4
+incorrect_4:
+	ld #0x0
+	st res
+	jump exit_4
+correct_4:	bns incorrect_4
+ 	ld res
+ 	inc
+ 	st res
+ 	jump exit_4
+exit_4:
+	pop
+	cla
+	ret
